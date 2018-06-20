40ae1e87c3c275d646e03d8ec6884a16c46d4a99
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-20 15:40:21 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      patriarch: {},
-      matriarch: {},
-      hero: {},
-      hero_freinds: [],
+        patriarch: {},
+        matriarch: {},
+        hero: {},
+        hero_freinds: [],
       }, 
     :capulet => {
       #your key/value pairs here!
